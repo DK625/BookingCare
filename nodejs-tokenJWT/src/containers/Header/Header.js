81bcdfd147ddb483b9b0750fa4ef5f91eb745387
your name <minhha10c8@gmail.com>
2023-03-01 17:22:28 +0700

    bomaythich

---
+++ nodejs-tokenJWT/src/containers/Header/Header.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from "../../store/actions";
+import Navigator from '../../components/Navigator';
+import { adminMenu } from './menuApp';
+import './Header.scss';
+import { handleLogoutApi } from '../../services/userService';
+// import { handleLogoutApi } from '../../services/userService';
+class Header extends Component {
+
+    handleLogout = async () => {
+        try {
+            let response = handleLogoutApi();
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    render() {
+        const { processLogout } = this.props;
+
+        return (
+            <div className="header-container">
+                {/* thanh navigator */}
+                <div className="header-tabs-container">
+                    <Navigator menus={adminMenu} />
+                </div>
+
+                {/* nút logout */}
+                <div className="btn btn-logout"
+                    onClick={processLogout}
+                // onClick={() => {
+                //     this.handleLogout();
+                //     // processLogout;
+                // }}
+                >
+                    <i className="fas fa-sign-out-alt"></i>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        processLogout: () => dispatch(actions.processLogout()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
